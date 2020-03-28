46c4be307971325d9f4bfcf38121825fb5e44248
Garima Dawar <garimadawar28@gmail.com>
2020-03-27 21:58:28 -0400

    Add files via upload
    A program that calculates sum and average of two numbers taking input from the user.

---
+++ DynamicAvg.py
@@ -0,0 +1,6 @@
+print("Calculate the sum and average of two numbers")
+number1 = float(input("Enter the first number"))
+number2 = float(input("Enter the second number"))
+
+print("the sum equals to " , number1 + number2)
+print("average equals to " , (number1 + number2) / 2)
