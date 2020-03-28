8e7445a150f727ce3b2fdf168c3e13ea638e7ee8
Garima Dawar <garimadawar28@gmail.com>
2020-03-27 22:08:43 -0400

    Add files via upload
    A Program that prints three different coddes from a given string of numbers

---
+++ PrintCode.py
@@ -0,0 +1,6 @@
+LotNum = input("Please Enter Number ")
+
+print("Country Code" , LotNum[0] + "" +  LotNum[1] + "" +  LotNum[2])
+
+print("Product Code" , LotNum[4] + "" +  LotNum[5] + "" +  LotNum[6] + "" +  LotNum[7])
+print("Batch Number " , LotNum[9] + "" +  LotNum[10] + "" +  LotNum[11] + "" +  LotNum[-2] + "" +  LotNum[-1])
