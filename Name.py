a5b3e9304ddc92e78044cb8ebde20467ae5eb9b5
Garima Dawar <garimadawar28@gmail.com>
2020-03-27 21:59:47 -0400

    Add files via upload
    A Program using strings datatype that takes First, Middle, Last NAme from the user and printing the initials.

---
+++ Name.py
@@ -0,0 +1,4 @@
+Fname = input("Please Enter Your First Name ")
+Mname = input("Please Enter Your Middle Name ")
+Lname = input("Please Enter Your Last Name ")
+print(Fname[0] + "" + Mname[0] + "" + Lname[0]);
